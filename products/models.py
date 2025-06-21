4aff9a458116d3f4f4c079d6125a417e740d9546
Leon Cai <leoncai@outlook.com>
2025-06-20 20:43:30 -0400

    model registration, first migration
    - basic admin panel model registration
    - fix null error in product model
    - make first migration

--- products/models.py
+++ products/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='products/', blank=True, null=True)
 
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="products")
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, related_name="products")
     tags = models.ManyToManyField(Tag, related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
