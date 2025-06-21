f7691a84e5dc669b034b43670f6272baa5cc824e
Leon Cai <leoncai@outlook.com>
2025-06-20 20:37:49 -0400

    model edits
    add default ordering, slug, image and stock fields

--- products/models.py
+++ products/models.py
@@ -8,6 +8,12 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -16,13 +22,31 @@ class Tag(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.TextField()
+    slug = models.SlugField(max_length=100, unique=True)
+
     description = models.TextField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="products")
     tags = models.ManyToManyField(Tag, related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=False)
+
+    stock = models.PositiveIntegerField(default=0)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
