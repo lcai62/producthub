686fe101d11ca30d48be4c643edbf11c1bc9945f
Leon Cai <leoncai@outlook.com>
2025-06-20 20:29:57 -0400

    create initial models
    create category, tag, and product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="products")
+    tags = models.ManyToManyField(Tag, related_name="products")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
