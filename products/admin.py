6118163ea9560c053fd2e6fdb4e21cca2d2b02d5
Leon Cai <leoncai@outlook.com>
2025-06-20 21:24:48 -0400

    improve admin panel, add sample data
    - customized admin panel
    - add sample data and media

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Category, Tag, Product
 
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'created_at')
+    list_filter = ('category', 'tags', 'created_at')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
